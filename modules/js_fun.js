b1993928b0db02abdda672cb7ac4b5c825012d45
secg-sr1 <32784662+secg-sr1@users.noreply.github.com>
2022-12-23 07:49:00 -0600

    Add files via upload

---
+++ modules/js_fun.js
@@ -0,0 +1,212 @@
+function show(id) {
+  let elem = document.getElementById(id);
+  if (elem.classList.contains("hidden")) {
+    // Keep active
+    elem.classList.toggle("hidden");
+    elem.classList.toggle("block");
+
+    let siblings = getSiblings(elem);
+    siblings.forEach(function (input) {
+      // Deactivate Siblings
+      if (input.classList.contains("block")) {
+        input.classList.toggle("hidden");
+        input.classList.toggle("block");
+      }
+    });
+  }
+  // else {
+  //   // Keep active
+  //   elem.classList.toggle("block");
+  //   elem.classList.toggle("hidden");
+  // }
+}
+
+function showHandles() {
+  var classes = [".dygraph-rangesel-fgcanvas", ".dygraph-rangesel-zoomhandle"];
+
+  var elems = document.querySelectorAll(classes);
+  elems.forEach(function (elem) {
+    elem.classList.toggle("visible");
+    elem.classList.toggle("invisible");
+  });
+  console.log(elems);
+}
+
+function blurIn() {
+  let blur = document.getElementById("base_blur");
+
+  if (!blur.classList.contains("blur_in")) {
+    blur.classList.toggle("blur_in");
+    blur.classList.toggle("blur_out");
+  }
+}
+
+function blurOut() {
+  let blur = document.getElementById("base_blur");
+
+  if (!blur.classList.contains("blur_out")) {
+    blur.classList.toggle("blur_in");
+    blur.classList.toggle("blur_out");
+  }
+}
+
+function getSiblings(e) {
+  let siblings = [];
+  if (!e.parentNode) {
+    return siblings;
+  }
+  // first child of the parent node
+  let sibling = e.parentNode.firstChild;
+  while (sibling) {
+    if (sibling.nodeType === 1 && sibling !== e) {
+      siblings.push(sibling);
+    }
+    sibling = sibling.nextSibling;
+  }
+  return siblings;
+}
+
+function getChild(e) {
+  let nodes = e.childNodes;
+  for (var i = 0; i < nodes.length; i++) {
+    if (nodes[i].nodeName.toLowerCase() == "div") {
+      nodes[i].style.background = color;
+    }
+  }
+}
+
+function tglActive(elem) {
+  if (!elem.classList.contains("active")) {
+    elem.classList.add("active");
+  }
+  // else {
+  //   elem.classList.remove("active");
+  // }
+
+  let siblings = getSiblings(elem);
+  siblings.forEach(function (input) {
+    input.classList.remove("active");
+  });
+}
+
+function resetMenu() {
+  resetBtns();
+  resetTabs();
+}
+
+function resetBtns() {
+  let btns = document.querySelectorAll(".head_btns");
+  btns.forEach(function (input) {
+    input.classList.remove("active");
+  });
+}
+
+function resetTabs() {
+  let blur = document.getElementById("base_blur");
+  let tabs = document.querySelectorAll(".tab");
+  tabs.forEach(function (input) {
+    input.classList.add("hidden");
+  });
+}
+
+function showTab(id) {
+  let tab;
+  let idsBlur = ["insights", "gathering"];
+  let blur = document.getElementById("base_blur");
+
+  // Reset setting
+  resetTabs();
+  blur.classList.add("blur_out");
+  blur.classList.remove("blur_in");
+
+  // Selected visibility
+  tab = document.getElementById(id);
+  tab.classList.remove("hidden");
+  tab.classList.add("block");
+
+  // If matching the idsBlur activate blur
+  if (idsBlur.indexOf(id) > -1) {
+    blur.classList.remove("blur_out");
+    blur.classList.add("blur_in");
+  }
+}
+
+function changePage(id) {
+  var reports_ids = ["report_1", "report_2", "report_3"];
+
+  reports_ids.forEach(function (input) {
+    let report = document.getElementById(input);
+    report.classList.add("hidden");
+  });
+  document.getElementById(id).classList.remove("hidden");
+}
+
+function activateLayer(id) {
+  let layers = document.getElementsByClassName("db_filter");
+  Array.from(layers).forEach(function (input) {
+    input.classList.remove("active");
+  });
+
+  let layer = document.getElementById(id);
+  layer.classList.add("active");
+}
+
+function changeProd(id) {
+  var card = {
+    img: "card_img",
+    name: "p_name",
+    days: "p_days",
+    price: "p_price",
+    gaze: "lineb_1",
+    focus: "lineb_2",
+    interaction: "lineb_3",
+  };
+
+  var products = [
+    {
+      name: "Productivity",
+      days: 35,
+      price: 65,
+      url: "/public/NEX_SALINAS_Productivity.png",
+      gaze: 40,
+      focus: 34,
+      interaction: 84,
+    },
+    {
+      name: "Social Interaction",
+      days: 12,
+      price: 15,
+      url: "/public/NEX_SALINAS_SocialInteraction.png",
+      gaze: 22,
+      focus: 56,
+      interaction: 34,
+    },
+    {
+      name: "Relaxing",
+      price: 20,
+      days: 42,
+      url: "/public/NEX_SALINAS_Relaxing.png",
+      gaze: 55,
+      focus: 43,
+      interaction: 67,
+    },
+  ];
+
+  let prod = products[id];
+  document.getElementById(card["img"]).src = prod["url"];
+  document.getElementById(card["name"]).innerHTML = prod["name"];
+  document.getElementById(card["price"]).innerHTML = String(
+    prod["price"]
+  ).concat(" %");
+  document.getElementById(card["days"]).innerHTML =
+    ["Average "] + String(prod["days"]) + " %";
+  document.getElementById(card["gaze"]).style.width = String(
+    prod["gaze"]
+  ).concat("%");
+  document.getElementById(card["focus"]).style.width = String(
+    prod["focus"]
+  ).concat("%");
+  document.getElementById(card["interaction"]).style.width = String(
+    prod["interaction"]
+  ).concat("%");
+}
---
+++ modules/three-octree-vertices.js
@@ -0,0 +1,139 @@
+import * as THREE from "three";
+
+export class Octree {
+  static _v = new THREE.Vector3();
+
+  constructor(vertices, maxVerticesPerNode = 8) {
+    if (vertices instanceof THREE.BufferGeometry) {
+      vertices = vertices.attributes.position.array;
+    }
+    this.maxVerticesPerNode = maxVerticesPerNode;
+    this.vertices = vertices;
+    const num = vertices.length / 3;
+    this.bounds = new THREE.Box3().setFromArray(vertices);
+    this.subTrees = [{ box: this.bounds, subTrees: [], indices: [] }];
+    if (num <= maxVerticesPerNode) {
+      this.subTrees[0].indices = Array(num)
+        .fill()
+        .map((_, i) => i);
+      return;
+    }
+
+    const boxes = Octree.boxSplit(this.bounds);
+    for (const b of boxes) {
+      const t = { box: b, subTrees: [], indices: [] };
+      for (let i = 0; i < num; ++i) {
+        if (b.containsPoint(Octree._v.fromArray(vertices, i * 3))) {
+          t.indices.push(i);
+        }
+      }
+      this.subTrees[0].subTrees.push(t);
+    }
+
+    const trees = this.subTrees[0].subTrees.slice();
+    while (trees.length) {
+      const curr = trees.at(-1);
+      trees.pop();
+      if (curr.indices.length > maxVerticesPerNode) {
+        this.splitCell(curr);
+        trees.push(...curr.subTrees);
+      }
+    }
+  }
+
+  addVertex(v) {
+    const newIndex = this.vertices.length / 3;
+    if (!this.vertices.push) {
+      this.vertices = Array.from(this.vertices);
+    }
+    this.vertices.push(v.x, v.y, v.z);
+    let curr = this.subTrees[0];
+    const found = false;
+    while (curr.subTrees.length) {
+      for (const t of curr.subTrees) {
+        if (t.box.containsPoint(v)) {
+          curr = t;
+          break;
+        }
+      }
+    }
+    curr.indices.push(newIndex);
+    if (curr.indices.length > this.maxVerticesPerNode) {
+      this.splitCell(curr);
+    }
+  }
+
+  search(position, radius) {
+    const sphere =
+      position instanceof THREE.Sphere
+        ? position
+        : new THREE.Sphere(position, radius);
+    const ret = [];
+    const trees = this.subTrees.slice();
+    while (trees.length) {
+      const curr = trees.at(-1);
+      trees.pop();
+      if (curr.box.intersectsSphere(sphere)) {
+        if (curr.subTrees.length) {
+          for (const t of curr.subTrees) {
+            trees.push(t);
+          }
+        } else {
+          for (const i of curr.indices) {
+            if (
+              sphere.containsPoint(Octree._v.fromArray(this.vertices, i * 3))
+            ) {
+              ret.push(i);
+            }
+          }
+        }
+      }
+    }
+    return ret;
+  }
+
+  mergeVertices(threshold = 1e-4) {
+    const sphere = new THREE.Sphere(undefined, threshold);
+    const verticesToIndices = [];
+    for (let i = 0, il = this.vertices.length / 3; i < il; ++i) {
+      if (verticesToIndices[i] === undefined) {
+        sphere.center.fromArray(this.vertices, i * 3);
+        const found = this.search(sphere);
+        const newIndex = Math.min(...found);
+        found.forEach((ind) => (verticesToIndices[ind] = newIndex));
+      }
+    }
+    return verticesToIndices;
+  }
+
+  splitCell(cell) {
+    const boxes = Octree.boxSplit(cell.box);
+    for (const b of boxes) {
+      const t = { box: b, subTrees: [], indices: [] };
+      for (const i of cell.indices) {
+        if (b.containsPoint(Octree._v.fromArray(this.vertices, i * 3))) {
+          t.indices.push(i);
+        }
+      }
+
+      cell.subTrees.push(t);
+    }
+    cell.indices = [];
+  }
+
+  static boxSplit(box) {
+    const boxes = [];
+    const halfsize = box.getSize(new THREE.Vector3()).multiplyScalar(0.5);
+    for (let x = 0; x < 2; x++) {
+      for (let y = 0; y < 2; y++) {
+        for (let z = 0; z < 2; z++) {
+          const curr = new THREE.Box3();
+          curr.min.set(x, y, z).multiply(halfsize).add(box.min);
+          curr.max.copy(curr.min).add(halfsize);
+          boxes.push(curr);
+        }
+      }
+    }
+    return boxes;
+  }
+}
